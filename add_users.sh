d2fddd1f8c29850b9aa77f3801a15659e53f7c32
Ravi Kushappanavar <ravi.k-ext@medseek.com>
2015-10-05 04:23:15 -0400

    fixed error

--- add_users.sh
+++ add_users.sh
@@ -4,6 +4,4 @@ docker exec rabbitmq rabbitmqctl add_user rabbitmq rabbitmq ; \
 
 docker exec rabbitmq rabbitmqctl set_user_tags rabbitmq administrator ; \
 
-docker exec rabbitmq rabbitmqctl set_permissions -p / rabbitmq ".*" ".*" ".*" ; \ 
-
-
+docker exec rabbitmq rabbitmqctl set_permissions -p / rabbitmq ".*" ".*" ".*" ;
