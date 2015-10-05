f95a63968de194e034deeca16d7f4df0ca27e471
Ravi Kushappanavar <ravi.k-ext@medseek.com>
2015-10-05 05:19:34 -0400

    removed add_users.sh

--- add_users.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker exec rabbitmq rabbitmqctl add_user rabbitmq rabbitmq ; \
-
-docker exec rabbitmq rabbitmqctl set_user_tags rabbitmq administrator ; \
-
-docker exec rabbitmq rabbitmqctl set_permissions -p / rabbitmq ".*" ".*" ".*" ;
